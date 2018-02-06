d8e467542173040ee81b7013f522aa91832d535e
shaibrana <34811272+shaibrana@users.noreply.github.com>
2018-02-06 13:44:55 +0400

    Update footer.php

--- wp-content/themes/illdy/footer.php
+++ wp-content/themes/illdy/footer.php
@@ -9,17 +9,20 @@
 <?php
 
 if ( current_user_can( 'edit_theme_options' ) ) {
-	$footer_copyright = get_theme_mod( 'illdy_footer_copyright', sprintf( __( '&copy; Copyright %s. All Rights Reserved.', 'illdy' ), date( 'Y' ) ) );
+	$footer_copyright  = get_theme_mod( 'illdy_footer_copyright', __( '&copy; Copyright 2016. All Rights Reserved.', 'illdy' ) );
 } else {
-	$footer_copyright = get_theme_mod( 'illdy_footer_copyright' );
+	$footer_copyright  = get_theme_mod( 'illdy_footer_copyright' );
 }
 ?>
-
-
-<?php if ( 'page' == get_option( 'show_on_front' ) && is_front_page() && get_theme_mod( 'illdy_go_to_top', false ) ) : ?>
-	<a href="#" class="illdy-top"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
-<?php endif ?>
-
+<footer id="footer">
+	<div class="bottom-footer">
+		<div class="container">
+			<p class="copyright">
+				<span class="bottom-copyright" data-customizer="copyright-credit"><?php echo illdy_sanitize_html( $footer_copyright ); ?></span>
+			</p>
+		</div>
+	</div>
+</footer><!--/#footer-->
 <?php wp_footer(); ?>
 </body>
 </html>
