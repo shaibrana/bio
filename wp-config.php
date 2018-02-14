9650363bd7f8c34a299d39cf21654f61885135b3
shaibrana <34811272+shaibrana@users.noreply.github.com>
2018-02-14 11:21:01 +0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'bio');
 define('DB_USER', 'wp_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'e68f3f5963ee4cb60a726e8c67');
+define('DB_PASSWORD', 'lbqqQT0E,LFD');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
